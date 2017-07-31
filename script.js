d936d3edca69e2fa8544b1040b20443663cb1229
ttn_js <ttn.js001@gmail.com>
2017-07-31 00:11:34 -0500

    cookies are storing, lets review local storage'

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log($);
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie() {
+    var user_input = getCookie("user_input");
+    if (user_input != "") {
+        $("#content").val(user_input);
+    } 
+} 
+
+$(document).ready(function() {
+
+	checkCookie();
+
+	$("#content").keyup(function(){
+		var cname = "user_input"
+		var cvalue = $("#content").val();
+
+	    setCookie(cname, cvalue);
+	});	
+});
+
