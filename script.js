f4bb37c0808f16d100a7a0ee94c415b91ea4b26e
ttn_js <ttn.js001@gmail.com>
2017-08-14 23:25:08 -0500

    jk still broken

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ console.log($);
 
 function setCookie(cname, cvalue) {
     document.cookie = cname + "=" + cvalue + ";";
+    console.log('firing');
 }
 
 function getCookie(cname) {
     var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
+    var ca = document.cookie;
+
+    console.log(ca);
+
+    // for(var i = 0; i < ca.length; i++) {
+    //     var c = ca[i];
+    //     while (c.charAt(0) == ' ') {
+    //         c = c.substring(1);
+    //     }
+    //     if (c.indexOf(name) == 0) {
+    //         return c.substring(name.length, c.length);
+    //     }
+    // }
+    // return "";
 }
 
 function checkCookie() {
@@ -29,23 +33,19 @@ function checkCookie() {
 
 $(document).ready(function() {
 
-    $('#content').change(function(){
-        var cname = "user_input"
-        var cvalue = $("#content").val();
-        setCookie(cname, cvalue);
-        console.log('new cookie set');
-    });
-
-    function InputChangeListener()
-    {
-        previousVal = $('#content').val();
-
-        if($('#content').val()!= previousVal){
-            $('#content').change();  
-        }
-    }
-
-    setInterval(InputChangeListener, 500);
+    checkCookie();
 
 });
 
+
+function settingNewCookie()
+{
+    // console.log('setting new cookie');
+
+    var cvalue = $("#content").val();
+    console.log(cvalue);
+    var cname = "user_input";
+    setCookie(cname, cvalue);
+}
+
+setInterval(settingNewCookie, 500);
